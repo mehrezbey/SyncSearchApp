e878f12098ecaf872640e8ec5351147d5aaf9bd4
mehrez bey <mahrezbey@gmail.com>
2024-07-17 16:53:49 +0100

    adding swagger

--- search_module/__init__.py
+++ search_module/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
+from flask_restx import Api
 
 from search_module.config import Config
 from search_module.utils.elasticsearch_synchronisation import synchronise,ingest_data_to_elasticsearch
@@ -11,13 +12,22 @@ Base = automap_base()
 def create_app(config_class = Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    api=Api(app,
+        title="Geoprod Search Module",
+        description="REST APIs",
+        doc='/',
+        version = "1.0"
+    )
     with app.app_context():
         db.init_app(app)
         Base.prepare(db.engine, reflect=True)
         synchronise(Base) # Add event listenners after each read, update and delete
         ingest_data_to_elasticsearch(db) # In case you will run the script for the first time, it will upload the database into elasticsearch
 
+    from search_module.main import main_namespace
+    api.add_namespace(main_namespace)
+    
     # Import the routes later to prevent circular import ( one of many solutions ) 
-    from search_module.main.routes import main
-    app.register_blueprint(main)
+    # from search_module.main.routes import main
+    # app.register_blueprint(main)
     return app
--- search_module/main/routes.py
+++ search_module/main/routes.py
@@ -1,50 +1,58 @@
 from flask import request, jsonify,Blueprint
+from flask_restx import fields , Resource
+
 import os
 from search_module.utils.elasticsearch_utils import query_index
 from search_module import Base,db
 from sqlalchemy.orm import sessionmaker
-
+from . import main_namespace
 
 database_name = os.getenv("DATABASE_NAME")
 
-main = Blueprint('main',__name__)
+# main = Blueprint('main',__name__)
 
-@main.route('/search')
-def search():
-    if(not request.args.get('table')):
-        return jsonify(message="Error! Table name is mandatory!"), 405
-    if(request.args.get('table') !="_all"):
-        index = database_name+"-"+ f"{request.args.get('table')}"
-    else: 
-        index = f"{request.args.get('table')}"
-    query = request.args.get('query', default='')
-    fields = request.args.get('fields',default='')
-    if(fields==''):
-        fields = ['*']
-    else:
-        fields = fields.split(",")
-    page = request.args.get('page',default = 1,type = int)
-    per_page = request.args.get('per_page',default = 5,type = int)
-    return query_index(index,query,fields,page,per_page)
 
-@main.route('/test')
-def test():
-    table = Base.classes.vips
-    Session = sessionmaker(bind=db.engine)
-    session = Session()
+#---------------------------- Swagger Models
+query_model  =main_namespace.model( 
+    'query_doc',{
+        'table':fields.String(required=True, description="The name of the table. Write '_all' if you want the search to include all tables."),
+        'query':fields.String(required=False,description="The text to search. If you want to return all documents, do not write anything."),
+        'fields':fields.String(required=False,description="The fields of the query seperated by ','. If you want to include all the fields do not write anything."),
+        'page':fields.Integer(required=False,description="The number of the page starting from 1. The default value = 1"),
+        'per_page':fields.Integer(required=False,description="The number of documents in each page. The default value = 5")
+    }
+)
 
-    new_row = table(vip_id=7, name='carlos')
-    session.add(new_row)
+@main_namespace.route('/search', methods=["POST"])
+class Search(Resource):
+    @main_namespace.expect(query_model)
+    def post(self):
+        req = request.get_json()
+        if(not ("table" in req)):
+            return jsonify(message="Error! Table name is mandatory!"), 405
+        if(req["table"] !="_all"):
+            index = database_name+"-"+ req["table"]
+        else: 
+            index = req["table"]
+        if("query" in req):
+            query = req["query"]
+        else:
+            query=""
+        if("fields" in req):
+            fields = req["fields"]
+        else:
+            fields=''
+        if(fields==''):
+            fields = ['*']
+        else:
+            fields = fields.split(",")
+        if("page" in req):
+            page = int(req["page"])
+        else:
+            page=1
+        if("per_page" in req):
+            per_page = int(req["per_page"])
+        else:
+            per_page=5
+        return query_index(index,query,fields,page,per_page)
 
-    new_row = table(vip_id=8, name='rog')
-    session.add(new_row)
-
-    existing_row = session.query(table).filter_by(vip_id=5).first()
-    existing_row.name = 'rafael'
-
-    edel = session.query(table).filter_by(vip_id=7).first()
-    session.delete(edel)
-
-    session.commit()
-    session.close()
-    return"" ,200
